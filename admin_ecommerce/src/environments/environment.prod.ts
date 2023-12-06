224a67cfc3507398aa7c7457fac3be357907dd0d
Bill Saravia <bill.saravia@tecsup.edu.pe>
2023-12-06 04:26:41 -0500

    first commit

---
+++ admin_ecommerce/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  appVersion: 'v723demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: false,
+  apiUrl: 'api',
+  URL_BACKEND: 'http://api.Tecshop.site/',
+  URL_SERVICIOS: 'http://api.Tecshop.site/api',
+  URL_FROTEND: 'http://admin.Tecshop.site/',
+  // apiUrl: 'mysite.com/api'
+};
---
+++ api_ecommerce/resources/Product.js
@@ -0,0 +1,44 @@
+export default {
+    product_list: (product, variedades = []) => {
+      // Check if product.galerias is defined before mapping over it
+      let GALERIAS = product.galerias ? product.galerias.map((galeria) => {
+        galeria.imagen = 'http://localhost:3000' + '/api/products/uploads/product/' + galeria.imagen;
+        return galeria;
+      }) : [];
+
+      // Check if product.portada is defined
+      const portadaUrl = product.portada ? 'http://localhost:3000' + '/api/products/uploads/product/' + product.portada : '';
+
+      // Use a safer method to get a random index in case GALERIAS is empty
+      const VAL = GALERIAS.length > 0 ? Math.floor(Math.random() * GALERIAS.length) : 0;
+      const IMAGEN_TWO = GALERIAS.length > 0 ? GALERIAS[VAL].imagen : '';
+
+      // Initialize avg_review, count_review, and CampaingDiscount
+      const avg_review = product.avg_review || 0;
+      const count_review = product.count_review || 0;
+      const CampaingDiscount = product.campaign_discount || 0;
+
+      return {
+        _id: product._id,
+        title: product.title,
+        sku: product.sku,
+        slug: product.slug,
+        imagen: portadaUrl,
+        categorie: product.categorie,
+        price_soles: product.price_soles,
+        price_usd: product.price_usd,
+        stock: product.stock,
+        description: product.description,
+        resumen: product.resumen,
+        tags: product.tags ? JSON.parse(product.tags) : [],
+        type_inventario: product.type_inventario,
+        state: product.state,
+        variedades: variedades,
+        imagen_two: IMAGEN_TWO,
+        galerias: GALERIAS,
+        avg_review: avg_review,
+        count_review: count_review,
+        campaing_discount: CampaingDiscount,
+      };
+    },
+  };
